2e0eb64371670a8a04a8a2641cfe21bbb37746ca
Daumantas Kavolis <12998363+dkavolis@users.noreply.github.com>
2019-06-20 20:55:09 +0100

    next release is Mach

--- FerramAerospaceResearch/FARGUI/FAREditorGUI/Simulation/RungeKutta.cs
+++ FerramAerospaceResearch/FARGUI/FAREditorGUI/Simulation/RungeKutta.cs
@@ -1,4 +1,4 @@
-﻿/*Ferram Aerospace Research v0.15.10.1 "Lundgren"
+﻿/*Ferram Aerospace Research v0.15.11.0 "Mach"
 =========================
 Aerodynamics model for Kerbal Space Program
 
--- FerramAerospaceResearch/FARPartGeometry/DebugVisualVoxelSubmesh.cs
+++ FerramAerospaceResearch/FARPartGeometry/DebugVisualVoxelSubmesh.cs
@@ -1,5 +1,5 @@
 /*
-Ferram Aerospace Research v0.15.10.1 "Lundgren"
+Ferram Aerospace Research v0.15.11.0 "Mach"
 =========================
 Copyright 2019, Daumantas Kavolis, aka dkavolis
 
