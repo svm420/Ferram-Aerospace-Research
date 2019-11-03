5bd236c0dca39f8efdd63b0271f9868b2d658313
Daumantas Kavolis <12998363+dkavolis@users.noreply.github.com>
2019-11-03 18:28:52 +0000

    make interface for ConfigValue

--- FerramAerospaceResearch/Configuration/ConfigValue.cs
+++ FerramAerospaceResearch/Configuration/ConfigValue.cs
@@ -1,19 +1,220 @@
+using System;
+using System.Text;
+using UnityEngine;
+
 namespace FerramAerospaceResearch
 {
-    public struct ConfigValue<T>
+    public interface IConfigValue
     {
+        string Name { get; }
+
+        string EditableName { get; }
+        void Reset();
+        void DebugString(StringBuilder sb);
+
+        void Parse(IConfigNode node);
+        void Save(IConfigNode node);
+    }
+
+    public interface IConfigValue<T> : IConfigValue
+    {
+        T Default { get; }
+        T Value { get; set; }
+    }
+
+    // abstract class since non-generic methods don't accept generic arguments
+    public abstract class ConfigValue<T> : IConfigValue<T>
+    {
+        private readonly Func<T, T> transform;
+
+        // using field for reference semantics
+        protected T value;
+
+        protected ConfigValue(string name, T value)
+        {
+            transform = null;
+            Name = name;
+            Default = value;
+            Value = value;
+        }
+
+        protected ConfigValue(string name, T value, Func<T, T> transform)
+        {
+            this.transform = transform;
+            Name = name;
+            Default = transform(value);
+            Value = value;
+        }
+
         public string Name { get; }
-        public T Value { get; }
+        public T Default { get; }
+
+        public T Value
+        {
+            get { return value; }
+            set { this.value = transform == null ? value : transform(value); }
+        }
+
+        public void Parse(IConfigNode node)
+        {
+            ParseValue(node);
+            Value = value;
+        }
+
+        protected abstract void ParseValue(IConfigNode node);
+
+        public abstract void Save(IConfigNode node);
 
         public string EditableName
         {
             get { return $"%{Name}"; }
         }
 
-        public ConfigValue(string name, T value)
+        public void Reset()
         {
-            Name = name;
-            Value = value;
+            Value = Default;
+        }
+
+        public void DebugString(StringBuilder sb)
+        {
+            sb.Append("    ").Append(Name).Append(": ").AppendLine(Value.ToString());
+        }
+
+        // implicit conversion to the stored value
+        public static implicit operator T(ConfigValue<T> cv)
+        {
+            return cv.Value;
+        }
+    }
+
+    // boilerplate for concrete types
+    public class IntConfigValue : ConfigValue<int>
+    {
+        public IntConfigValue(string name, int value) : base(name, value)
+        {
+        }
+
+        public IntConfigValue(string name, int value, Func<int, int> transform) : base(name, value, transform)
+        {
+        }
+
+        protected override void ParseValue(IConfigNode node)
+        {
+            node.TryGetValue(Name, ref value);
+        }
+
+        public override void Save(IConfigNode node)
+        {
+            node.AddValue(Name, Value);
+        }
+    }
+
+    public class StringConfigValue : ConfigValue<string>
+    {
+        public StringConfigValue(string name, string value) : base(name, value)
+        {
+        }
+
+        public StringConfigValue(string name, string value, Func<string, string> transform) :
+            base(name, value, transform)
+        {
+        }
+
+        protected override void ParseValue(IConfigNode node)
+        {
+            node.TryGetValue(Name, ref value);
+        }
+
+        public override void Save(IConfigNode node)
+        {
+            node.AddValue(Name, Value);
+        }
+    }
+
+    public class FloatConfigValue : ConfigValue<float>
+    {
+        public FloatConfigValue(string name, float value) : base(name, value)
+        {
+        }
+
+        public FloatConfigValue(string name, float value, Func<float, float> transform) : base(name, value, transform)
+        {
+        }
+
+        protected override void ParseValue(IConfigNode node)
+        {
+            node.TryGetValue(Name, ref value);
+        }
+
+        public override void Save(IConfigNode node)
+        {
+            node.AddValue(Name, Value);
+        }
+    }
+
+    public class ColorConfigValue : ConfigValue<Color>
+    {
+        private static readonly char[] separators = {',', ' ', ';'};
+
+        public ColorConfigValue(string name, Color value) : base(name, value)
+        {
+        }
+
+        protected override void ParseValue(IConfigNode node)
+        {
+            string color = string.Empty;
+            node.TryGetValue(Name, ref color);
+            Value = ReadColor(color);
+        }
+
+        public override void Save(IConfigNode node)
+        {
+            node.AddValue(Name, SaveColor(Value));
+        }
+
+        private static Color ReadColor(string input)
+        {
+            string[] splitValues = input.Split(separators);
+
+            int curIndex = 0;
+            var color = new Color {a = 1};
+            foreach (string s in splitValues)
+            {
+                if (s.Length <= 0)
+                    continue;
+                if (!float.TryParse(s, out float val))
+                    continue;
+                switch (curIndex)
+                {
+                    case 0:
+                        color.r = val;
+                        break;
+                    case 1:
+                        color.g = val;
+                        break;
+                    default:
+                        color.b = val;
+                        return color;
+                }
+
+                curIndex++;
+            }
+
+            return color;
+        }
+
+        private static string SaveColor(Color color)
+        {
+            var builder = new StringBuilder();
+
+            //Should return string in format of color.r, color.g, color.b
+            builder.Append(color.r);
+            builder.Append(",");
+            builder.Append(color.g);
+            builder.Append(",");
+            builder.Append(color.b);
+
+            return builder.ToString();
         }
     }
 }
