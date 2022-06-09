9b8e2a9003d7afd561dddbcf265b5d57ce0b1640
dkavolis <12998363+dkavolis@users.noreply.github.com>
2022-06-09 18:47:38 +0300

    use EqualityComparer to compare two values without boxing

--- FerramAerospaceResearch.Base/Utils/FARUtils.cs
+++ FerramAerospaceResearch.Base/Utils/FARUtils.cs
@@ -1,48 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 namespace FerramAerospaceResearch
 {
+    internal class GenericValueEqualityComparer<T> : EqualityComparer<T> where T : struct, IEquatable<T>
+    {
+        public override bool Equals(T x, T y) => x.Equals(y);
+        public override int GetHashCode(T obj) => obj.GetHashCode();
+        public override bool Equals(object obj) => obj is GenericValueEqualityComparer<T>;
+        public override int GetHashCode() => GetType().Name.GetHashCode();
+    }
+
+    internal static class CustomEqualityComparer<T>
+    {
+        public static EqualityComparer<T> Default { get; } = CreateComparer();
+
+        private static EqualityComparer<T> CreateComparer()
+        {
+            Type t = typeof(T);
+
+            // handle equatable value types differently since C# boxes them for comparisons, enums are compared without boxing
+            if (!typeof(IEquatable<T>).IsAssignableFrom(t) || !t.IsValueType || t.IsEnum)
+                return EqualityComparer<T>.Default;
+
+            // the workarounds that are needed since C# doesn't support overloads by constraints...
+            Type comparerType = typeof(GenericValueEqualityComparer<>).MakeGenericType(t);
+            return (EqualityComparer<T>)Activator.CreateInstance(comparerType);
+        }
+    }
+
     public static class FARUtils
     {
         public static bool ValuesEqual<T>(T a, T b)
         {
-            return a switch
-            {
-                IComparable<T> comparable => (comparable.CompareTo(b) == 0),
-                IEquatable<T> equatable => equatable.Equals(b),
-                null => b is null,
-                IComparable comparable => (comparable.CompareTo(b) == 0),
-                _ => a.Equals(b)
-            };
-        }
-
-        public static bool ValuesEqual<T>(object a, T b)
-        {
-            return a switch
-            {
-                IComparable<T> comparable => (comparable.CompareTo(b) == 0),
-                IEquatable<T> equatable => equatable.Equals(b),
-                null => b is null,
-                IComparable comparable => (comparable.CompareTo(b) == 0),
-                _ => a.Equals(b)
-            };
-        }
-
-        public static bool ValuesEqual(object a, object b)
-        {
-            return a switch
-            {
-                IComparable comparable => (comparable.CompareTo(b) == 0),
-                null => b is null,
-                _ => a.Equals(b)
-            };
+            return CustomEqualityComparer<T>.Default.Equals(a, b);
         }
 
         public static void Swap<T>(ref T a, ref T b)
         {
-            T c = b;
-            b = a;
-            a = c;
+            (b, a) = (a, b);
         }
     }
 }
